1b3f9c6d38466c397a165af88a5b0e4d4d83f8fa
Alexandr <sachatsvetkov@gmail.com>
2025-05-16 20:09:25 +0300

    АПИ НОвая почта, надо доделать

--- orders/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import create_order
-
-app_name = 'orders'
-
-urlpatterns = [
-    path('create/', views.create_order, name='create_order')
-]
